3b539cd5b33b17b54e9d825eb7e5e5a7d920e406
PaulaEPR <paulaperera@gmail.com>
2022-01-25 19:14:59 +0100

    translate species

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,14 +1,29 @@
 import defaultImage from '../images/default_image.jpg';
 
 const CharacterCard = (props) => {
-  const image = props.character.image === '' ? defaultImage : props.character.image;
+  const image =
+    props.character.image === '' ? defaultImage : props.character.image;
+
+  const species = () => {
+    if (props.character.species === 'human') {
+      return 'Humano';
+    } else if (props.character.species === 'half-giant') {
+      return 'Semi gigante';
+    } else if (props.character.species === 'werewolf') {
+      return 'Hombre lobo';
+    } else if (props.character.species === 'ghost') {
+      return 'Fantasma';
+    } else {
+      return props.character.species;
+    }
+  };
   return (
     <>
-    <img className="character__image" src={image} alt="" />
-    <h3 className="character__name">{props.character.name}</h3>
-    <p className="character__species">{props.character.species}</p>
+      <img className="character__image" src={image} alt="" />
+      <h3 className="character__name">{props.character.name}</h3>
+      <p className="character__species">{species()}</p>
     </>
   );
-}
+};
 
-export default CharacterCard
+export default CharacterCard;
